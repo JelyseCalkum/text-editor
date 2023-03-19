a423bae3a9cc40184a8cb7de647388cf3a57decf
jelysecalkum <jelyse.lauren@gmail.com>
2023-03-19 13:40:45 -0700

    added login to put and get data from db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,48 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+console.log('Post to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+//Creates a connection to the database and version we want to use.
+const jateDb = await openDB('jate', 1);
+
+//Creates a new transaction and specifies the database and privileges.
+const tx = jateDb.transaction('jate', 'readwrite');
+
+//Opens up the desired object store.
+const store = tx.objectStore('jate');
+
+//Use the .add() method on the store and pass in content.
+const request = store.put({ id: 1, value: content });
+
+//Gets conirmation of the request.
+const result = await request;
+HTMLFormControlsCollection.log('🚀 - data saved to the database', result);
+};
+
+//Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  //Open the 'jate' database with version 1
+  const jateDb = await openDB('jate', 1);
+
+  //Start a new transaction in read-only mode
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  //Retrieve the 'jate' object strore
+  const store = tx.objectStore('jate');
+
+  //Retrieve all data from the object store 
+  const request = store.getAll();
+
+  //Wait for the request to complete and get the result
+  const result = await request;
+  console.log('result.value', result);
+  //return retrieved data
+  return result;
+};
 
 initdb();
